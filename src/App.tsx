828bed4af0d7f6048bcb53e228956975de5fa86c
Urszula Ziemska <urszula.ziemska-ext@bshg.com>
2023-06-29 12:43:25 +0200

    adding database connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const mongoose = require("mongoose");
+
+  // Set up MongoDB connection
+  const uri =
+    "mongodb+srv://cluster0.h9pnzaf.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority";
+
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = mongoose.connection;
+
+  db.once("open", () => {
+    console.log("Connected to MongoDB database");
+  });
+
+  db.on("error", (err: Error) => {
+    console.error("MongoDB connection error:", err);
+  });
 
   return (
     <>
@@ -29,7 +49,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
